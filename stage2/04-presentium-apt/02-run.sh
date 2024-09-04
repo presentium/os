b4773c00f09646fc87060ce38d0e5ac99d9ff4c3
Christophe - CC4 <roulin43@gmail.com>
2024-09-05 01:46:40 +0200

    fix: bad systemd service path

--- stage2/04-presentium-apt/02-run.sh
+++ stage2/04-presentium-apt/02-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 on_chroot <<- \EOF
-	ln -s '/lib/systemd/system/presentium-client.service' '/etc/systemd/system/multi-user.target.wants/presentium-client.service'
+	ln -s '/etc/systemd/system/presentium-client.service' '/etc/systemd/system/multi-user.target.wants/presentium-client.service'
 EOF
