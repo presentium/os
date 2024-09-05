0bc8589238c59862ca92539f38652377dc14cf35
Christophe - CC4 <roulin43@gmail.com>
2024-09-05 07:43:17 +0200

    fix: i2c activation

--- stage2/07-enable-i2c/00-run.sh
+++ stage2/07-enable-i2c/00-run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-raspi-config nonint do_i2c 0
-EOF
+echo "i2c-dev" >> "${ROOTFS_DIR}/etc/modules"
+echo "SUBSYSTEM==\"i2c-dev\",KERNEL==\"i2c-[0-9]+\", GROUP=\"i2c\", MODE=\"0660\"" >> "${ROOTFS_DIR}/lib/udev/rules.d/60-i2c-tools.rules"
