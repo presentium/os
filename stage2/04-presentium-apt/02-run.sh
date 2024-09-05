792e494871081441a2a1603a74af8817c966ff2c
Christophe - CC4 <roulin43@gmail.com>
2024-09-05 07:33:03 +0200

    fix: symlink to enable systemd service

--- stage2/04-presentium-apt/02-run.sh
+++ stage2/04-presentium-apt/02-run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-on_chroot <<- \EOF
-	ln -s '/etc/systemd/system/presentium-client.service' '/etc/systemd/system/multi-user.target.wants/presentium-client.service'
-EOF
+ln -s ${ROOTFS_DIR}/etc/systemd/system/presentium-client.service ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/presentium-client.service
