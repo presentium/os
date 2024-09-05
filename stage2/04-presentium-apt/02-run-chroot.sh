bd300a29aa73b155af52dbe2f0acf3644c62b3a9
Christophe - CC4 <roulin43@gmail.com>
2024-09-05 17:16:09 +0200

    fix: presentium-client apt

---
+++ stage2/04-presentium-apt/02-run-chroot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+ln -s /etc/systemd/system/presentium-client.service /etc/systemd/system/multi-user.target.wants/presentium-client.service
--- stage2/04-presentium-apt/02-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-ln -s ${ROOTFS_DIR}/etc/systemd/system/presentium-client.service ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/presentium-client.service
